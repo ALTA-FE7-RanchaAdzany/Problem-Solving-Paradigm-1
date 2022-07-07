78034f704f89064a4f9c4aeb273954525c63eb14
ikram abdillah <ikram.koli@gmail.com>
2022-07-07 14:05:39 +0700

    Task Done

--- Part1/problem1/problem1.js
+++ Part1/problem1/problem1.js
@@ -1,5 +1,19 @@
+// Simple Equations
+
 function simpleEquations(a, b, c) {
-  // Your code here
+  for (let i = 1; i <= c; i++) {
+    for (let j = 1; j <= b; j++) {
+      for (let k = 1; k <= a; k++) {
+        if (i + j + k === a && i * j * k === b && i * i + j * j + k * k === c) {
+          console.log(i, j, k);
+          return;
+        }
+      }
+    }
+  }
+  console.log("no solution");
 }
 
+simpleEquations(1, 2, 3); // no solution
+simpleEquations(6, 6, 14); // 1 2 3
 module.exports = simpleEquations;
